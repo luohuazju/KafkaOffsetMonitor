b29f57b95c8094000f9020947870c209f59f462a
Carl Luo <cluo@jobs2careers.com>
2015-06-08 16:13:44 -0500

    add the merge strategy to fix the assembly issue

--- project/Build.scala
+++ project/Build.scala
@@ -13,6 +13,7 @@ object KafkaUtilsBuild extends Build {
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
     retrieveManaged := true,
     transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
+    mergeStrategy in assembly := mergeFirst,
     resolvers ++= Seq(
       "sonatype-snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
       "sonatype-releases" at "http://oss.sonatype.org/content/repositories/releases",
@@ -29,6 +30,44 @@ object KafkaUtilsBuild extends Build {
 
   lazy val offsetmonitor = Project("offsetmonitor", file("."), settings = offsetmonSettings)
 
+  lazy val mergeFirst: String => MergeStrategy = {
+      case PathList("javax", "servlet", xs @ _*) => MergeStrategy.first
+      case PathList("org", "apache", "jasper", xs @ _*) => MergeStrategy.first
+      case PathList("org", "fusesource", xs @ _*) => MergeStrategy.first
+      case PathList("org", "apache", "commons", xs @ _*) => MergeStrategy.first
+      case PathList("org", "apache", "commons", "beanutils", xs @ _*) => MergeStrategy.first
+      case PathList("org", "apache", "commons", "collections", xs @ _*) => MergeStrategy.first
+      case PathList("com", "esotericsoftware", "minlog", xs @ _*) => MergeStrategy.first
+      case PathList("org", "eclipse", xs @ _*) => MergeStrategy.first
+      case PathList("META-INF", xs @ _*) =>
+        (xs map {_.toLowerCase}) match {
+          case ("changes.txt" :: Nil ) =>
+            MergeStrategy.discard
+          case ("manifest.mf" :: Nil) | ("eclipsef.rsa" :: Nil) | ("index.list" :: Nil) | ("dependencies" :: Nil) =>
+            MergeStrategy.discard
+          case ps @ (x :: xs) if ps.last.endsWith(".sf") || ps.last.endsWith(".dsa") || ps.last.endsWith("pom.properties") || ps.last.endsWith("pom.xml") =>
+            MergeStrategy.discard
+          case "plexus" :: xs =>
+            MergeStrategy.discard
+          case "services" :: xs =>
+            MergeStrategy.filterDistinctLines
+          case ("spring.schemas" :: Nil) | ("spring.handlers" :: Nil) =>
+            MergeStrategy.filterDistinctLines
+          case ps @ (x :: xs) if ps.last.endsWith(".jnilib") || ps.last.endsWith(".dll") =>
+            MergeStrategy.first
+          case ps @ (x :: xs) if ps.last.endsWith(".txt") =>
+            MergeStrategy.discard
+          case ("notice" :: Nil) | ("license" :: Nil) | ("mailcap" :: Nil )=>
+            MergeStrategy.discard
+          case _ => MergeStrategy.deduplicate
+        }
+      case "application.conf" => MergeStrategy.concat
+      case "about.html" => MergeStrategy.discard
+      case "plugin.properties" => MergeStrategy.first
+      case _ => MergeStrategy.first
+    }
+
+
   def offsetmonSettings = sharedSettings ++ Seq(
     name := "KafkaOffsetMonitor",
     libraryDependencies ++= Seq(
@@ -43,3 +82,4 @@ object KafkaUtilsBuild extends Build {
       "java m2" at "http://download.java.net/maven/2",
       "twitter repo" at "http://maven.twttr.com"))
 }
+
